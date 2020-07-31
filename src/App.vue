5e62c8a5528c1116a387180a3f234267bd580265
jjh204 <59932565+jjh204@users.noreply.github.com>
2020-07-31 19:21:14 +0200

    first commit for new project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,195 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <!-- adding my navbar information for portfolio, github etc -->
+
+    <div class="navbar">
+      <md-toolbar md-elevation="1">
+        <a class="md-title" style="flex: 1" href="/">
+          <img src="./assets/hobbs_logo.jpg" class="navbar-img" alt="Logo stating Jennifer Hobbs" />
+        </a>
+        <a href="/" role="menuitem" class="navigation-list_item">Refresh</a>
+        <a
+          href="https://jjh204.github.io/portfolio-website"
+          target="_blank"
+          role="menuitem"
+          class="navigation-list_item"
+        >
+          Developer
+          Portfolio
+        </a>
+        <a
+          href="https://github.com/jjh204"
+          target="_blank"
+          role="menuitem"
+          class="navigation-list_item"
+        >Github</a>
+      </md-toolbar>
+    </div>
+
+    <!-- List input and initial view -->
+
+    <div class="todo-container">
+      <md-field class="todo-input">
+        <md-input
+          v-model="currentTodo"
+          @keydown.enter="addTodo()"
+          placeholder="add new list item"
+          class="todo-input-area"
+        ></md-input>
+        <md-button class="md-fab md-mini" v-model="currentTodo" v-on:click="addTodo">
+          <md-icon>add</md-icon>
+        </md-button>
+      </md-field>
+
+      <!-- then the todo list actually displayed -->
+
+      <md-card class="card-list" v-if="displayList()">
+        <md-card-content>
+          <md-list class="todos">
+            <md-subheader>THINGS THAT I MIGHT DO TODAY:</md-subheader>
+
+            <md-list-item v-for="todo in todos" :key="todo.id">
+              <input type="checkbox" v-model="todo.completed" class="checkbox" />
+              <span
+                class="input-list-item"
+                v-on:dblclick="editTodo(todo)"
+                v-show="editTodoId !== todo.id"
+                :class="{ completed: todo.completed }"
+              >{{ todo.label }}</span>
+              <input
+                v-model="todo.label"
+                v-show="editTodoId == todo.id"
+                v-on:keydown.enter="saveTodo"
+                class="input-edit-item"
+              />
+              <md-button
+                class="md-fab md-mini"
+                v-show="editTodoId !== todo.id"
+                @click="editTodo(todo)"
+              >
+                <md-icon>edit</md-icon>
+              </md-button>
+              <md-button
+                class="md-fab md-mini"
+                size="small"
+                v-show="editTodoId == todo.id"
+                @click="saveTodo"
+              >
+                <md-icon>save</md-icon>
+              </md-button>
+              <md-button class="md-fab md-mini" @click="removeTodo(todo)">
+                <md-icon>cancel</md-icon>
+              </md-button>
+            </md-list-item>
+          </md-list>
+        </md-card-content>
+      </md-card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editTodoId: null,
+    };
+  },
+  methods: {
+    displayList() {
+      return this.todos.length > 0;
+    },
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      this.editTodoId = todo.id;
+    },
+    saveTodo() {
+      this.editTodoId = null;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #e5e9ed;
+  color: #042a54;
+}
+
+.navbar {
+  background: #f8f9fa;
+}
+
+.navbar-img {
+  max-width: 180px;
+}
+
+.navbar-img:hover,
+.navbar-img:focus {
+  opacity: 0.8;
+}
+
+a {
+  color: #042a54;
+  cursor: pointer;
+  margin-right: 2em;
+  padding-right: 2em;
+}
+
+.container {
+  position: center;
+}
+
+.todo-input {
+  max-width: 40em;
+  padding-bottom: 0.8em;
+  border-bottom: 1px solid rgba(4, 42, 84, 0.3);
+}
+
+.todo-input-area {
+  margin-left: 6em;
+}
+
+.checkbox {
+  width: 20px;
+  height: 20px;
+}
+
+.card-list {
+  margin: 0 auto;
+  background: #ccd4dc;
+  border-radius: 10px;
+  max-width: 60em;
+}
+
+.input-list-item {
+  width: 75%;
+}
+
+.input-edit-item {
+  width: 75%;
+  background: #e5e9ed;
+  border: none;
+  font-size: 1em;
+  color: #042a54;
+}
+
+@media all and (max-width: 750px) {
+  .navbar-img {
+    max-width: 80px;
+  }
 }
 </style>
